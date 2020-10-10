5d5304ba6ded172fca1308d71843b2984960c269
Ayser Jamshidi <acejamshidi4th@gmail.com>
2020-10-10 01:26:23 -0400

    Bug fix and code cleanup
    -Added "swap" as a possible suffix that Chegg will use as the overlay's div name.
    -Changed everything into a for each loop for some cleanliness and for ease of future additions of possible suffixes or elements.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 window.addEventListener("load", function(){
-	 // Delete the device registration overlay
-	document.getElementById("cs-dm-add").remove();
+	var possibleSuffix = ["add", "swap"];
+	var scrollBarElements = ["html", "body"]
+	
+	// Find the proper device registration overlay
+	possibleSuffix.forEach(function(currentSuffix) {
+		var currentSuffixElement = document.getElementById("cs-dm-" + currentSuffix);
+		
+		// Delete overlay if it exists
+		if (currentSuffixElement != null)
+			currentSuffixElement.remove();
+	});
+	 
 
 	// Force vertical/horizontal scrollbar on the page
-	document.getElementsByTagName('html')[0].style.setProperty('overflow', 'visible', 'important');
-	document.getElementsByTagName('body')[0].style.setProperty('overflow', 'visible', 'important');
+	scrollBarElements.forEach( function(currentElement) {
+		document.getElementsByTagName(currentElement)[0].style.setProperty('overflow', 'visible', 'important');
+	});
 });
